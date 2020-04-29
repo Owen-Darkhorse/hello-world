bc0aa07cba52e266eba2901dfbaed4a5435d3f47
owenwang <jingxinwang25@gmail.com>
2020-04-29 14:42:39 -0400

    modified some files

--- static/javascript/javastyles.js
+++ static/javascript/javastyles.js
@@ -1,4 +1,8 @@
+/* let eventTarget = document.getElementById('name');
+eventTarget.onClick = newStyle; */
+
 function newStyle() {
+  /* console.log("in function new style") */
   let color = '';
   let font = '';
   var element = document.getElementById('name');
@@ -6,10 +10,11 @@ function newStyle() {
   element.style.fontFamily = "Arial";
 			}
 			
+			
 function moreHobbies() {
   let cooking = document.createElement('li');
   cooking.innerHTML = "Cooking";
-  
+ 
   let violin = document.createElement('li');
   violin.innerHTML = "Violin";
   
@@ -23,11 +28,10 @@ function moreHobbies() {
 			
 function moreLinks(){
 	let GitHub = document.createElement('li');
-	GitHub.innerHTML = "github";
-	//GitHub.innerHTML = "https://github.com/Owen-Darkhorse";
-	
+	GitHub.innerHTML = "<a href='https://github.com/Owen-Darkhorse'>GitHub</a>";
+ 	
 	let Stack_Overflow = document.createElement('li');
-	Stack_Overflow.innerHTML = "https://stackoverflow.com/users/12638805/owen-darkhorse";
+	Stack_Overflow.innerHTML = "<a href='https://stackoverflow.com/users/12638805/owen-darkhorse'>Stack Overflow</a>";
 	
 	document.getElementById('social media').appendChild(GitHub);
 	document.getElementById('social media').appendChild(Stack_Overflow);
@@ -35,3 +39,17 @@ function moreLinks(){
 	let text = document.getElementById('more links');
 	document.getElementById('Contacts').removeChild(text);
 }
+
+
+let media = document.getElementById("social-media");
+media.style.display = 'none';
+
+let buttonShare = document.getElementById('share-button');
+
+function clickToshow(){
+	media.style.display = 'block';
+	//buttonShare.innerHTML = "Please contact me";
+	document.getElementById('Contacts').removeChild(buttonShare)
+}
+
+buttonShare.onclick = clickToshow;
